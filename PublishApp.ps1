c266e78aba482213a758d64963cffe15d9e4d101
SeminarTraining <jk@navilogic.dk>
2018-11-30 14:34:22 +0100

    Lab 6

--- PublishApp.ps1
+++ PublishApp.ps1
@@ -1,15 +1,15 @@
 Set-ExecutionPolicy RemoteSigned
 # Import the module for the  cmdLet
-Import-Module 'C:\Program Files\Microsoft Dynamics NAV\110\Service\NavAdminTool.ps1'
+Import-Module 'C:\Program Files\Microsoft Dynamics 365 Business Central\130\Service\NavAdminTool.ps1'
 # Publish app to database
-Publish-NAVApp -ServerInstance DynamicsNAV110 -Path "C:\Users\Admin\Desktop\D. E. Veloper_Seminar Module_2.0.0.0.app" -SkipVerification
+Publish-NAVApp -ServerInstance BC130 -Path "C:\Users\Admin\Desktop\ALCode\Seminar\Default publisher_Seminar_1.0.0.0.app" -SkipVerification
 # Install in tenant
-Install-NAVApp -ServerInstance DynamicsNAV110 -Name "Seminar Module"
+Install-NAVApp -ServerInstance BC130 -Name "Seminar Module"
 # Uninstall the app - Delete the data
-Uninstall-NAVApp  -ServerInstance DynamicsNAV110 -Name "Seminar Module" -DoNotSaveData 
+Uninstall-NAVApp  -ServerInstance BC130 -Name "Seminar Module" -DoNotSaveData 
 # Remove the app from the database - but keep the tables
-Unpublish-NAVApp -ServerInstance DynamicsNAV110 -Name "Seminar Module" 
+Unpublish-NAVApp -ServerInstance BC130 -Name "Seminar Module" 
 # Synchronize the objekts in theh tenant
-Sync-NAVTenant -ServerInstance DynamicsNAV110
+Sync-NAVTenant -ServerInstance BC130
 # Remove the tables if the app is gone
-Sync-NAVApp -ServerInstance DynamicsNAV110 -Name "Seminar Module" -Mode Clean
+Sync-NAVApp -ServerInstance BC130 -Name "Seminar Module" -Mode Clean
